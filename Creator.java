13cb82b833c3f98eec2ba116249ef2bed6796e5c
Ruslan <izya.man18@gmail.com>
2024-06-22 13:03:55 +0300

    create creator and function to add new animal

---
+++ Creator.java
@@ -0,0 +1,41 @@
+import Animals.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Creator {
+    private static List<String> allAnimals = new ArrayList<>(Arrays.asList("Dog", "Cat", "Hamster", "Horse", "Camel", "Donkey"));
+
+    public static Animal createAnimal(int animalType, String name, List<String> commands) {
+        Animal animal = new Dog(name, commands);
+
+        switch (allAnimals.get(animalType)) {
+            case "Dog":
+                animal = new Dog(name, commands);
+                break;
+            case "Cat":
+                animal = new Cat(name, commands);
+                break;
+            case "Hamster":
+                animal = new Hamster(name, commands);
+                break;
+            case "Horse":
+                animal = new Horse(name, commands);
+                break;
+            case "Camel":
+                animal = new Camel(name, commands);
+                break;
+            case "Donkey":
+                animal = new Donkey(name, commands);
+                break;
+        }
+
+        return animal;
+    }
+
+
+    public static List<String> getAllAnimalsToCreate() {
+        return new ArrayList<>(allAnimals);
+    }
+}
--- Menu.java
+++ Menu.java
@@ -1,3 +1,74 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+import Animals.Animal;
+
 public class Menu {
-    public void start() {}
+    private List<Animal> animals = new ArrayList<>();
+
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
+
+        boolean isWorking = true;
+
+        while (isWorking) {
+            System.out.println("""
+                    Выведите команду:
+                    1. Завести новое животное
+                    2. Показать список комманд
+                    3. Обучить команде
+                    4. Выход""");
+
+            int userCommand = scanner.nextInt();
+
+            switch (userCommand) {
+                case (1):
+                    createAnimal();
+                    break;
+                case (2):
+                    showCommands();
+                    break;
+                case (3):
+                    addCommand();
+                    break;
+                case (4):
+                    isWorking = false;
+                    break;
+            }
+
+        }
+    }
+
+    private void createAnimal() {
+        Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Введите номер животного из списка, которого хотели бы завести");
+
+            List<String> allAnimalsToCreate = Creator.getAllAnimalsToCreate();
+
+            for (int i = 0; i < allAnimalsToCreate.size(); i++) {
+                System.out.println(i + " " + allAnimalsToCreate.get(i));
+            }
+
+            int animalType = scanner.nextInt();
+
+            if (animalType < 0 || animalType >= allAnimalsToCreate.size()) {
+                System.out.println("Выбирете корректный номер из списка");
+                return;
+            }
+
+            System.out.println("Введите кличку животного");
+            String animalName = scanner.next();
+
+            System.out.println("Введите, через запятую, команды, которые знает животное");
+            List<String> animalCommands = Arrays.stream(scanner.next().split(",")).toList();
+
+            animals.add(Creator.createAnimal(animalType, animalName, animalCommands));
+
+    }
+    
+    private void showCommands() {}
+    private void addCommand() {}
 }
